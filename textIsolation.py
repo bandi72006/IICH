6f353dadf79a60e77cf4fd828d3bdae8b1a94133
Gonzilo <tcatoriginal@gmail.com>
2021-10-13 13:10:26 +0400

    img is now in grayscale in order to reduce processing time

--- textIsolation.py
+++ textIsolation.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 import sys
 
-img = cv.imread(cv.samples.findFile("C:\\Users\\97152\\Source\\Repos\\bandi72006\\IICH\\Images\\testText.png"))
+img = cv.cvtColor(cv.imread(cv.samples.findFile("C:\\Users\\97152\\Source\\Repos\\bandi72006\\IICH\\Images\\testText2.png")), cv.COLOR_BGR2GRAY)
 
 if img is None:
     print("image could not be read")
 
 cv.imshow("display", img)
-k = cv.waitKey(0)
+cv.waitKey(0)
